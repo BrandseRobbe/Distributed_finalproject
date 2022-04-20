69538f7f5a0ab845bf5361a8c65a2bd3ec7b4223
Robbe Brandse <robbe.brandse@gmail.com>
2022-04-20 11:15:32 +0200

    template node

---
+++ api/src/main/java/Node/Bootstrap.java
@@ -0,0 +1,7 @@
+package Node;
+
+public class Bootstrap {
+    public static void dostuff(){
+
+    }
+}
--- api/src/main/java/Node/Discovery.java
+++ api/src/main/java/Node/Discovery.java
@@ -3,13 +3,12 @@ package Node;
 import java.io.IOException;
 
 public class Discovery {
-    public static void main(String[] args) throws IOException {
+    public static void Discover() throws IOException {
         MulticastPublisher multicastPublisher = new MulticastPublisher();
         MulticastReceiver multicastReceiver = new MulticastReceiver();
         multicastReceiver.run();
         multicastPublisher.multicast("hello");
 
         multicastReceiver.run();
-
     }
 }
